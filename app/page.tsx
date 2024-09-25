28c55e93ea1fe318547f9a0f4425012ef9251d17
nam <namish@tuta.io>
2024-09-25 14:15:18 +0530

    floww

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]"></div>
   );
 }
