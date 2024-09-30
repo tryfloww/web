a109edda4cb42fade926afd4076868e2d8bb0645
nam <namish@tuta.io>
2024-09-30 18:03:41 +0530

    team member

--- app/(pages)/channel/[slug]/Info.tsx
+++ app/(pages)/channel/[slug]/Info.tsx
@@ -1,6 +1,6 @@
 import { PersonIcon } from '@radix-ui/react-icons';
 export const Info = () => {
-  return <div className="info bg-secondary dark:bg-secondary/30 p-6  rounded-xl">
+  return <div className="info bg-secondary w-full dark:bg-secondary/30 p-6  rounded-xl">
     <div className="flex gap-3 items-end">
       <img src="/default.png" alt="" className="h-28 w-28 rounded-full" />
       <div className="flex flex-col gap-[4px]">
