78a69199f2f7bd41d8ebbb141bf985b4de0bb51d
nam <namish@tuta.io>
2024-10-11 12:25:30 +0530

    init tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+  plugins: [TanStackRouterVite({}), react()],
+  define: {
+    'process.env': process.env
+  }
+})
