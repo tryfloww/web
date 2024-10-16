025898f78213ad0acfa2202db8a58eee523926a8
nam <namish@tuta.io>
2024-10-16 17:28:27 +0530

    init solid app;fck react completely

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,7 @@
+import { A } from "@solidjs/router";
+
+export default function NotFound() {
+  return (
+    <div class="h-screen w-screen">not foudn</div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,7 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
-  component: HomeComponent,
-})
-
-function HomeComponent() {
+import { A } from "@solidjs/router";
+export default function Home() {
   return (
-    <div className="bg-neutral-950 min-h-screen pt-20 p-8 text-white h-full p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+    <main class="text-center mx-auto text-gray-700 p-4">
+    </main>
+  );
 }
